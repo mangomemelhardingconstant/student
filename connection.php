7f617ed50b98e7b1b2f545d22602d578f0a9df21
CONSTI <63623556+mangomemelhardingconstant@users.noreply.github.com>
2020-07-18 05:56:52 +0200

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,10 @@
+ <?php
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "student";
+
+	$con = mysqli_connect($servername, $username, $password ,$dbname);
+	 
+ ?>	
---
+++ presence.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Attendance</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<form action="login.php form-container">
+    <div class="container-fluid">
+    <h1>attendance</h1>
+    </div>
+    <div class="alert alert-success">
+    <strong>signé!   </strong>bonne suite de cours.
+    </div>
+    <a href="login.php" class="btn btn-info" role="button">Retour</a>
+
+</form>
+
+</body>
+</html>
